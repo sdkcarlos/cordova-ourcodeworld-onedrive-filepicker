2db601c0947d336817983c852d637a21cfefc7d3
Carlos Delgado <sdkcarlos@outlook.com>
2016-02-16 18:55:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cordova Our Code World SFTP Plugin for android
+
+# The plugin is ready and can be used in production.
+
+#[Visit the homepage to read the documentation](https://sdkcarlos.github.io/sites/cordova-sftp-plugin.html)
+
+# Installation
+
+Get the plugin into your android project with :
+
+```batch
+cordova plugin add https://github.com/sdkcarlos/cordova-ourcodeworld-sftpplugin.git
+```
+
---
+++ src/android/OurCodeWorldOneDriveFilePicker.java
@@ -0,0 +1,17 @@
+package com.ourcodeworld.plugins.onedrivefilepicker;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.microsoft.onedrivesdk.picker.*;
+
+
+public class OurCodeWorldOneDriveFilePicker extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        
+        
+    }
+}
