ffc6c1e28842bcfd5202e6ae1ea13a2d71edeff7
Carlos Delgado <sdkcarlos@outlook.com>
2016-05-02 19:31:19 +0200

    Update

--- src/android/OurCodeWorldOneDriveFilePicker.java
+++ src/android/OurCodeWorldOneDriveFilePicker.java
@@ -8,10 +8,34 @@ import com.microsoft.onedrivesdk.picker.*;
 
 
 public class OurCodeWorldOneDriveFilePicker extends CordovaPlugin {
+    private static final String ONEDRIVE_APP_ID = "0000000048188688";
+    private static final String ACTION_SHOWPICKER = "showpicker";
+
+    private IPicker mPicker;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        final JSONObject arg_object = data.getJSONObject(0);
+
+        if (ACTION_SHOWPICKER.equals(action)) {
+            mPicker = Picker.createPicker(ONEDRIVE_APP_ID);
+            mPicker.startPicking(cordova.getActivity(), LinkType.DownloadLink);
+        }
 
         return true;
+    }
+
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        // Get the results from the picker
+        IPickerResult result = mPicker.getPickerResult(requestCode, resultCode, data);
+        // Handle the case if nothing was picked
+        if (result != null) {
+            // Do something with the picked file
+            Log.d("main", "Link to file '" + result.getName() + ": " + result.getLink());
+            return;
+        }
+
+        // Handle non-OneDrive picker request
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- www/ourcodeworldonedrive.js
+++ www/ourcodeworldonedrive.js
@@ -1,7 +1,26 @@
 /*global cordova, module*/
 
 module.exports = {
-    hello: function(){
-        return true;
+    createOneDriveManager: function(){
+        var _settings = {
+            appId : null
+        };
+
+        return {
+            setAppId: function(id){
+                _settings.appId = id;
+            },
+            showPicker: function(){
+                //if(!_settings.appId){
+                //    throw new Error("You need to give an onedriveapp id, use setAppId method");
+                //}
+
+                cordova.exec(function(data){
+                    console.info(data);
+                }, function(err){
+                    console.error(err);
+                }, "OurCodeWorldOneDrive", "showpicker", [_settings]);
+            }
+        };
     }
 };
