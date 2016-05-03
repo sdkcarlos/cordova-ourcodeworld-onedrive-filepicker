ca469bbc9bdf711fcc6ec7c5b54dc2432f5cb425
Carlos Delgado <sdkcarlos@outlook.com>
2016-05-03 18:19:55 +0200

    Fix

--- src/android/DisplayMessageActivity.java
+++ src/android/DisplayMessageActivity.java
@@ -11,34 +11,5 @@ import android.widget.Toast;// ToToast
 import com.microsoft.onedrivesdk.picker.*;
 
 public class DisplayMessageActivity extends CordovaActivity{
-    private CallbackContext activityResultCallback = null;
-    private boolean activityResultKeepRunning = true;
-    private boolean keepRunning = true;
-
-    @Override
-    public void setActivityResultCallback(CordovaPlugin plugin) {
-        this.activityResultCallback = plugin;
-    }
-
-    public void startActivityForResult(CordovaPlugin command, Intent intent, int requestCode) {
-        this.activityResultCallback = command;
-        this.activityResultKeepRunning = this.keepRunning;
-
-        // If multitasking turned on, then disable it for activities that return results
-        if (command != null) {
-            this.keepRunning = false;
-        }
-
-        // Start activity
-        super.startActivityForResult(intent, requestCode);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        CordovaPlugin callback = this.activityResultCallback;
-        if (callback != null) {
-            callback.onActivityResult(requestCode, resultCode, intent);
-        }
-    }
+    
 }
