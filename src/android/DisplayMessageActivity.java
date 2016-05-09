96c230480378befc64a4c5872d7343d849f3a4ee
Carlos Delgado <sdkcarlos@outlook.com>
2016-05-09 13:40:41 +0200

    Fix

--- src/android/DisplayMessageActivity.java
+++ src/android/DisplayMessageActivity.java
@@ -14,13 +14,14 @@ public class DisplayMessageActivity extends Activity{
     private static String ONEDRIVE_APP_ID = "0000000048188688";
     private IPicker mPicker;
 
-    public void onCreate()
-    {
-       super.onCreate();
+    public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.main);
        tolog("In the onCreate() event");
        mPicker = Picker.createPicker(ONEDRIVE_APP_ID);
        mPicker.startPicking(this, LinkType.DownloadLink);
     }
+
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
